46e4247e66fbdc29c0dff9ae9e504143aa0c798e
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-02 19:39:39 +0700

    Add static method headers for CIDR calculator

--- app/src/main/java/com/wtfart/ipaddressmanager/utils/Cidr.kt
+++ app/src/main/java/com/wtfart/ipaddressmanager/utils/Cidr.kt
@@ -6,5 +6,25 @@ package com.wtfart.ipaddressmanager.utils
 class Cidr {
     companion object {
 
+        @JvmStatic
+        fun computeAddressBitsCombination(numberOfRequestedAddresses: Int) = arrayOf(0)
+
+        @JvmStatic
+        fun computeInitialIpAddresses(addressBitsCombination: Array<Int>) = arrayOf("")
+
+        @JvmStatic
+        fun computeCidrNotation(ipAddress: String, numberOfMaskBits: Int) = ""
+
+        @JvmStatic
+        fun computeCidrNetmask(numberOfMaskBits: Int) = ""
+
+        @JvmStatic
+        fun computeWildcardMask(numberOfAddressBits: Int) = ""
+
+        @JvmStatic
+        fun computeAvailableIpAddresses(ipAddress: String, numberOfAddressBits: Array<Int>) = arrayOf("")
+
+        @JvmStatic
+        fun compute(ipAddress: String, numberOfRequestedAddresses: Int) = arrayOf(Cidr())
     }
 }
