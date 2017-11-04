f9deafd4081b8f67651db42451a8361fca2cd195
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-05 02:11:42 +0700

    Implement initial IP addresses computation

--- app/src/main/java/com/wtfart/ipaddressmanager/utils/Cidr.kt
+++ app/src/main/java/com/wtfart/ipaddressmanager/utils/Cidr.kt
@@ -11,7 +11,28 @@ class Cidr {
         fun computeAddressBitsCombination(numberOfRequestedAddresses: Int) = arrayOf(0)
 
         @JvmStatic
-        fun computeInitialIpAddresses(ipAddress: Long, addressBitsCombination: Array<Int>) = arrayOf(0b0)
+        fun computeInitialIpAddresses(ipAddress: Long, addressBitsCombination: Array<Int>): Array<Long> {
+            val reducedIpAddress =
+                    ipAddress and IntRange(1, 32)
+                            .fold(StringBuilder()) { bitStr, bit ->
+                                bitStr.append(
+                                        if (bit <= 32 - addressBitsCombination[0]) {
+                                            '1'
+                                        } else {
+                                            '0'
+                                        }
+                                )
+                            }.toString()
+                            .toLong(2)
+            return (0 until addressBitsCombination.size).map { address ->
+                        reducedIpAddress + (0 until address).fold(0L) { offset, index ->
+                            offset + Math.pow(
+                                    2.0,
+                                    addressBitsCombination[index].toDouble()
+                            ).toLong()
+                        }
+                    }.toTypedArray()
+        }
 
         @JvmStatic
         fun computeCidrNotation(ipAddress: String, numberOfMaskBits: Int) = ""
