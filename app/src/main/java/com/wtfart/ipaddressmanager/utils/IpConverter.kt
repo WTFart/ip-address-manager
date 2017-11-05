630848661a85c66e6abdd9376b630efafcd17b54
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-05 14:10:53 +0700

    Create IP Converter class

---
+++ app/src/main/java/com/wtfart/ipaddressmanager/utils/IpConverter.kt
@@ -0,0 +1,16 @@
+package com.wtfart.ipaddressmanager.utils
+
+/**
+ * Created by mickeycj on 11/5/2017 AD.
+ */
+class IpConverter {
+
+    companion object {
+
+        @JvmStatic
+        fun toBinary(ipAddress: String) = 0b0
+
+        @JvmStatic
+        fun toIpAddress(binary: Long) = ""
+    }
+}
