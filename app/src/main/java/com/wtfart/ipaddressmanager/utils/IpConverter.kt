f1c6ca92e6bf98aecd24d48473c9d4c5fb13d746
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-06 09:40:13 +0700

    Implement IP address to binary converter

--- app/src/main/java/com/wtfart/ipaddressmanager/utils/IpConverter.kt
+++ app/src/main/java/com/wtfart/ipaddressmanager/utils/IpConverter.kt
@@ -8,7 +8,10 @@ class IpConverter {
     companion object {
 
         @JvmStatic
-        fun toBinary(ipAddress: String) = 0b0
+        fun toBinary(ipAddress: String) = ipAddress.split('.')
+                .foldIndexed(0b0L) { index, binary, byte ->
+                    binary + byte.toLong() * Math.pow(2.0, (24 - 8 * index).toDouble()).toLong()
+                }
 
         @JvmStatic
         fun toIpAddress(binary: Long) = ""
