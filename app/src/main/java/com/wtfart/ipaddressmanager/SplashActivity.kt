2d7bf96c75f616d25ca1ada0f66a19e9017ef137
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-07 15:27:44 +0700

    Create handler to simulate 'loading' in SplashActivity

--- app/src/main/java/com/wtfart/ipaddressmanager/SplashActivity.kt
+++ app/src/main/java/com/wtfart/ipaddressmanager/SplashActivity.kt
@@ -2,11 +2,32 @@ package com.wtfart.ipaddressmanager
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+
+    private val DELAY_TIME = 3000L
+
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        mHandler = Handler()
+        mRunnable = Runnable {
+            TODO("Implement an entry point for the next activity - MainActivity")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mHandler.postDelayed(mRunnable, DELAY_TIME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mHandler.removeCallbacks(mRunnable)
     }
 }
