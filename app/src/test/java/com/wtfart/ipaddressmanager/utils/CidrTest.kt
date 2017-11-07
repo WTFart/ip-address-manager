9c76e441c5110be104aceb9687ae92a3f2228ccd
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-07 11:40:08 +0700

    Refactor CIDR netmask test cases

--- app/src/test/java/com/wtfart/ipaddressmanager/utils/CidrTest.kt
+++ app/src/test/java/com/wtfart/ipaddressmanager/utils/CidrTest.kt
@@ -475,102 +475,119 @@ class CidrTest {
     @Test
     fun `16 mask bits should result in 255|255|0|0 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(16)
+
         assertEquals(0b11111111111111110000000000000000, cidrNetmask)
     }
 
     @Test
     fun `17 mask bits should result in 255|255|128|0 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(17)
+
         assertEquals(0b11111111111111111000000000000000, cidrNetmask)
     }
 
     @Test
     fun `18 mask bits should result in 255|255|192|0 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(18)
+
         assertEquals(0b11111111111111111100000000000000, cidrNetmask)
     }
 
     @Test
     fun `19 mask bits should result in 255|255|224|0 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(19)
+
         assertEquals(0b11111111111111111110000000000000, cidrNetmask)
     }
 
     @Test
     fun `20 mask bits should result in 255|255|240|0 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(20)
+
         assertEquals(0b11111111111111111111000000000000, cidrNetmask)
     }
 
     @Test
     fun `21 mask bits should result in 255|255|248|0 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(21)
+
         assertEquals(0b11111111111111111111100000000000, cidrNetmask)
     }
 
     @Test
     fun `22 mask bits should result in 255|255|252|0 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(22)
+
         assertEquals(0b11111111111111111111110000000000, cidrNetmask)
     }
 
     @Test
     fun `23 mask bits should result in 255|255|254|0 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(23)
+
         assertEquals(0b11111111111111111111111000000000, cidrNetmask)
     }
 
     @Test
     fun `24 mask bits should result in 255|255|255|0 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(24)
+
         assertEquals(0b11111111111111111111111100000000, cidrNetmask)
     }
 
     @Test
     fun `25 mask bits should result in 255|255|255|128 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(25)
+
         assertEquals(0b11111111111111111111111110000000, cidrNetmask)
     }
 
     @Test
     fun `26 mask bits should result in 255|255|255|192 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(26)
+
         assertEquals(0b11111111111111111111111111000000, cidrNetmask)
     }
 
     @Test
     fun `27 mask bits should result in 255|255|255|224 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(27)
+
         assertEquals(0b11111111111111111111111111100000, cidrNetmask)
     }
 
     @Test
     fun `28 mask bits should result in 255|255|255|240 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(28)
+
         assertEquals(0b11111111111111111111111111110000, cidrNetmask)
     }
 
     @Test
     fun `29 mask bits should result in 255|255|255|248 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(29)
+
         assertEquals(0b11111111111111111111111111111000, cidrNetmask)
     }
 
     @Test
     fun `30 mask bits should result in 255|255|255|252 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(30)
+
         assertEquals(0b11111111111111111111111111111100, cidrNetmask)
     }
 
     @Test
     fun `31 mask bits should result in 255|255|255|254 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(31)
+
         assertEquals(0b11111111111111111111111111111110, cidrNetmask)
     }
 
     @Test
     fun `32 mask bits should result in 255|255|255|255 CIDR netmask`() {
         val cidrNetmask = Cidr.computeNetmask(32)
+
         assertEquals(0b11111111111111111111111111111111, cidrNetmask)
     }
 
