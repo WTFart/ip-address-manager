6e909f5bb09f43a619ee49432bb7742038d281a3
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-07 12:21:15 +0700

    Refactor binary to IP address converter test cases

--- app/src/test/java/com/wtfart/ipaddressmanager/utils/IpConverterTest.kt
+++ app/src/test/java/com/wtfart/ipaddressmanager/utils/IpConverterTest.kt
@@ -220,151 +220,211 @@ class IpConverterTest {
 
     @Test
     fun `Binary representation of 11000000000111100000000000000000 should result in an IP address of 192|30|0|0`() {
-        assertEquals("192.30.0.0", IpConverter.toIpAddress(0b11000000000111100000000000000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000000000000000)
+
+        assertEquals("192.30.0.0", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110000000000 should result in an IP address of 192|30|12|0`() {
-        assertEquals("192.30.12.0", IpConverter.toIpAddress(0b11000000000111100000110000000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110000000000)
+
+        assertEquals("192.30.12.0", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110000001000 should result in an IP address of 192|30|12|8`() {
-        assertEquals("192.30.12.8", IpConverter.toIpAddress(0b11000000000111100000110000001000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110000001000)
+
+        assertEquals("192.30.12.8", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110000001110 should result in an IP address of 192|30|12|14`() {
-        assertEquals("192.30.12.14", IpConverter.toIpAddress(0b11000000000111100000110000001110))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110000001110)
+
+        assertEquals("192.30.12.14", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110000001111 should result in an IP address of 192|30|12|15`() {
-        assertEquals("192.30.12.15", IpConverter.toIpAddress(0b11000000000111100000110000001111))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110000001111)
+
+        assertEquals("192.30.12.15", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110000010000 should result in an IP address of 192|30|12|16`() {
-        assertEquals("192.30.12.16", IpConverter.toIpAddress(0b11000000000111100000110000010000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110000010000)
+
+        assertEquals("192.30.12.16", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110000010010 should result in an IP address of 192|30|12|18`() {
-        assertEquals("192.30.12.18", IpConverter.toIpAddress(0b11000000000111100000110000010010))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110000010010)
+
+        assertEquals("192.30.12.18", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110001000000 should result in an IP address of 192|30|12|64`() {
-        assertEquals("192.30.12.64", IpConverter.toIpAddress(0b11000000000111100000110001000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110001000000)
+
+        assertEquals("192.30.12.64", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110001100000 should result in an IP address of 192|30|12|96`() {
-        assertEquals("192.30.12.96", IpConverter.toIpAddress(0b11000000000111100000110001100000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110001100000)
+
+        assertEquals("192.30.12.96", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110001101000 should result in an IP address of 192|30|12|104`() {
-        assertEquals("192.30.12.104", IpConverter.toIpAddress(0b11000000000111100000110001101000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110001101000)
+
+        assertEquals("192.30.12.104", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110001110000 should result in an IP address of 192|30|12|112`() {
-        assertEquals("192.30.12.112", IpConverter.toIpAddress(0b11000000000111100000110001110000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110001110000)
+
+        assertEquals("192.30.12.112", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000110001111000 should result in an IP address of 192|30|12|120`() {
-        assertEquals("192.30.12.120", IpConverter.toIpAddress(0b11000000000111100000110001111000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000110001111000)
+
+        assertEquals("192.30.12.120", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000111000000000 should result in an IP address of 192|30|14|0`() {
-        assertEquals("192.30.14.0", IpConverter.toIpAddress(0b11000000000111100000111000000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000111000000000)
+
+        assertEquals("192.30.14.0", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000111001000000 should result in an IP address of 192|30|14|64`() {
-        assertEquals("192.30.14.64", IpConverter.toIpAddress(0b11000000000111100000111001000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000111001000000)
+
+        assertEquals("192.30.14.64", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000111001001000 should result in an IP address of 192|30|14|72`() {
-        assertEquals("192.30.14.72", IpConverter.toIpAddress(0b11000000000111100000111001001000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000111001001000)
+
+        assertEquals("192.30.14.72", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000111001001100 should result in an IP address of 192|30|14|76`() {
-        assertEquals("192.30.14.76", IpConverter.toIpAddress(0b11000000000111100000111001001100))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000111001001100)
+
+        assertEquals("192.30.14.76", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100000111001001110 should result in an IP address of 192|30|14|78`() {
-        assertEquals("192.30.14.78", IpConverter.toIpAddress(0b11000000000111100000111001001110))
+        val binary = IpConverter.toIpAddress(0b11000000000111100000111001001110)
+
+        assertEquals("192.30.14.78", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100001000000000000 should result in an IP address of 192|30|16|0`() {
-        assertEquals("192.30.16.0", IpConverter.toIpAddress(0b11000000000111100001000000000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100001000000000000)
+
+        assertEquals("192.30.16.0", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100001000100000000 should result in an IP address of 192|30|17|0`() {
-        assertEquals("192.30.17.0", IpConverter.toIpAddress(0b11000000000111100001000100000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100001000100000000)
+
+        assertEquals("192.30.17.0", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100001000101000000 should result in an IP address of 192|30|17|64`() {
-        assertEquals("192.30.17.64", IpConverter.toIpAddress(0b11000000000111100001000101000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100001000101000000)
+
+        assertEquals("192.30.17.64", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100001000101010000 should result in an IP address of 192|30|17|80`() {
-        assertEquals("192.30.17.80", IpConverter.toIpAddress(0b11000000000111100001000101010000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100001000101010000)
+
+        assertEquals("192.30.17.80", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100001000101010100 should result in an IP address of 192|30|17|84`() {
-        assertEquals("192.30.17.84", IpConverter.toIpAddress(0b11000000000111100001000101010100))
+        val binary = IpConverter.toIpAddress(0b11000000000111100001000101010100)
+
+        assertEquals("192.30.17.84", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100001000101010110 should result in an IP address of 192|30|17|86`() {
-        assertEquals("192.30.17.86", IpConverter.toIpAddress(0b11000000000111100001000101010110))
+        val binary = IpConverter.toIpAddress(0b11000000000111100001000101010110)
+
+        assertEquals("192.30.17.86", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100010000000000000 should result in an IP address of 192|30|32|0`() {
-        assertEquals("192.30.32.0", IpConverter.toIpAddress(0b11000000000111100010000000000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100010000000000000)
+
+        assertEquals("192.30.32.0", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100010100000000000 should result in an IP address of 192|30|40|0`() {
-        assertEquals("192.30.40.0", IpConverter.toIpAddress(0b11000000000111100010100000000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100010100000000000)
+
+        assertEquals("192.30.40.0", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100010100100000000 should result in an IP address of 192|30|41|0`() {
-        assertEquals("192.30.41.0", IpConverter.toIpAddress(0b11000000000111100010100100000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100010100100000000)
+
+        assertEquals("192.30.41.0", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100010100110000000 should result in an IP address of 192|30|41|128`() {
-        assertEquals("192.30.41.128", IpConverter.toIpAddress(0b11000000000111100010100110000000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100010100110000000)
+
+        assertEquals("192.30.41.128", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100010100110100000 should result in an IP address of 192|30|41|160`() {
-        assertEquals("192.30.41.160", IpConverter.toIpAddress(0b11000000000111100010100110100000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100010100110100000)
+
+        assertEquals("192.30.41.160", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100010100110110000 should result in an IP address of 192|30|41|176`() {
-        assertEquals("192.30.41.176", IpConverter.toIpAddress(0b11000000000111100010100110110000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100010100110110000)
+
+        assertEquals("192.30.41.176", binary)
     }
 
     @Test
     fun `Binary representation of 11000000000111100010100110111000 should result in an IP address of 192|30|41|184`() {
-        assertEquals("192.30.41.184", IpConverter.toIpAddress(0b11000000000111100010100110111000))
+        val binary = IpConverter.toIpAddress(0b11000000000111100010100110111000)
+
+        assertEquals("192.30.41.184", binary)
     }
 }
