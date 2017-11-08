e99adc1d0436358d8689dc7223a6508ed13d56fb
oatThanut <oat_thanut@hotmail.com>
2017-11-08 22:03:02 +0700

    Initial CalculatorFragment

---
+++ app/src/main/java/com/wtfart/ipaddressmanager/CalculatorFragment.kt
@@ -0,0 +1,23 @@
+package com.wtfart.ipaddressmanager
+
+import android.app.Fragment
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CalculatorFragment : Fragment() {
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
