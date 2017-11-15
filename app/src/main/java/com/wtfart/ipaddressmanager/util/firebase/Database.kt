b5a2b19f7bb7276f24f6c41eaa5890da7d16ca1d
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-15 17:04:59 +0700

    Finish IP Address Registration

--- app/src/main/java/com/wtfart/ipaddressmanager/util/firebase/Database.kt
+++ app/src/main/java/com/wtfart/ipaddressmanager/util/firebase/Database.kt
@@ -4,6 +4,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.wtfart.ipaddressmanager.model.Cidr
 
 import com.wtfart.ipaddressmanager.model.Network
 import com.wtfart.ipaddressmanager.model.NetworkRepository
@@ -15,32 +16,41 @@ class Database {
 
     companion object {
 
+        private val networkRepository = NetworkRepository.repository
+
         private val USERS_KEY = "users"
 
         @JvmStatic
         fun retrieveIpAddresses(uid: String) {
-            val networkRepository = NetworkRepository.repository
+            getDatabaseReference(uid).addValueEventListener(object : ValueEventListener {
 
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    networkRepository.clear()
+
+                    dataSnapshot
+                            .children
+                            .mapNotNullTo(networkRepository.networks) { network ->
+                                network.getValue<Network>(Network::class.java)
+                            }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                }
+            })
+        }
+
+        @JvmStatic
+        fun registerIpAddress(uid: String, name: String, cidrNotations: Array<Cidr>) {
+            val key = getDatabaseReference(uid).push().key
+
+            getDatabaseReference(uid).child(key).setValue(Network(key, name, cidrNotations))
+        }
+
+        private fun getDatabaseReference(uid: String) =
             FirebaseDatabase
                     .getInstance()
                     .reference
                     .child(USERS_KEY)
                     .child(uid)
-                    .addValueEventListener(object : ValueEventListener {
-
-                        override fun onDataChange(dataSnapshot: DataSnapshot) {
-                            networkRepository.clear()
-
-                            dataSnapshot
-                                    .children
-                                    .mapNotNullTo(networkRepository.networks) { network ->
-                                        network.getValue<Network>(Network::class.java)
-                                    }
-                        }
-
-                        override fun onCancelled(databaseError: DatabaseError) {
-                        }
-                    })
-        }
     }
 }
