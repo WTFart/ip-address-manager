a69cd1feb24395f09353d61b5b883ded2c99819d
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-15 17:03:30 +0700

    Update Network class implementation

--- app/src/main/java/com/wtfart/ipaddressmanager/model/Network.kt
+++ app/src/main/java/com/wtfart/ipaddressmanager/model/Network.kt
@@ -1,6 +1,28 @@
 package com.wtfart.ipaddressmanager.model
 
+import java.util.Arrays
+
 /**
  * Created by mickeycj on 11/15/2017.
  */
-data class Network(val id: String, val name: String, val cidrNotations: MutableList<Cidr>)
+data class Network(val id: String, val name: String, val cidrNotations: Array<Cidr>) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Network
+
+        return id == other.id
+                && name == other.name
+                && Arrays.equals(cidrNotations, other.cidrNotations)
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + Arrays.hashCode(cidrNotations)
+
+        return result
+    }
+}
