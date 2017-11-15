0d35b547ef289837b76400fd30c6e4ca4814e417
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-15 16:38:02 +0700

    Update NetworkRepository class

--- app/src/main/java/com/wtfart/ipaddressmanager/model/NetworkRepository.kt
+++ app/src/main/java/com/wtfart/ipaddressmanager/model/NetworkRepository.kt
@@ -3,4 +3,15 @@ package com.wtfart.ipaddressmanager.model
 /**
  * Created by mickeycj on 11/15/2017.
  */
-data class NetworkRepository(val networks: MutableList<Network>)
+data class NetworkRepository(val networks: MutableList<Network> = mutableListOf()) {
+
+    companion object {
+
+        @JvmField
+        val repository = NetworkRepository()
+    }
+
+    fun clear() {
+        networks.clear()
+    }
+}
