01a6c4e4155a2fa78d0a7a7804ecdf9a6e828382
Chanon Jenakom <chanonjenakom@gmail.com>
2017-11-19 21:25:31 +0700

    Update method name

--- app/src/main/java/com/wtfart/ipaddressmanager/util/firebase/Auth.kt
+++ app/src/main/java/com/wtfart/ipaddressmanager/util/firebase/Auth.kt
@@ -14,7 +14,7 @@ class Auth {
     companion object {
 
         @JvmStatic
-        fun logInUser(
+        fun loginUser(
                 activity: AppCompatActivity,
                 email: String,
                 password: String,
